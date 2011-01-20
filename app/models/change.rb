78361f4c98d752c06145f66c3e37476952f4b244
Fonsan <fonsan@gmail.com>
2011-01-20 12:59:06 +0100

    Snyggade till allt lite

--- app/models/change.rb
+++ app/models/change.rb
@@ -1,6 +1,7 @@
 require 'ipaddr'
 class Change < ActiveRecord::Base
     attr_accessible :person_id, :ip, :points
+    belongs_to :person
     
     def nice_ip
       IPAddr.new(ip,Socket::AF_INET).to_s
